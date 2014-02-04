8436542b6c3ee767db76232f06cd71fb5be3b791
Shaun Weston <shaun.weston@splicegroup.co.nz>
2014-02-05 11:01:36 +1300

    Updating b.bat files.

---
+++ Examples/RestoreAGSSite.bat
@@ -0,0 +1,10 @@
+REM ----- Restore ArcGIS Server Site -----
+C:\Python27\ArcGIS10.2\python "C:\Development\Projects\ArcGIS Admin Toolkit\BackupRestoreAGSSite.py" ^
+ "http://Laptop-SFW:6080/arcgis" ^
+ "siteadmin" ^
+ "adm1n" ^
+ "Restore" ^
+ "" ^
+ "C:\Downloads\GISSERVER.agssite" ^
+ "False" ^
+ "C:\Downloads\Report.txt"
