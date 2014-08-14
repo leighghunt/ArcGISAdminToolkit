21deea044c7d72a461fa74e575ff0861a900a2bf
Leigh Hunt <leigh@venari.co.nz>
2014-08-15 10:52:21 +1200

    Just trailing whitespace

--- CacheMapService.py
+++ CacheMapService.py
@@ -30,18 +30,18 @@ emailMessage = ""
 output = None
 
 # Start of main function
-def mainFunction(mapService,updateMode,tileScheme): # Get parameters from ArcGIS Desktop tool by seperating by comma e.g. (var1 is 1st parameter,var2 is 2nd parameter,var3 is 3rd parameter)  
+def mainFunction(mapService,updateMode,tileScheme): # Get parameters from ArcGIS Desktop tool by seperating by comma e.g. (var1 is 1st parameter,var2 is 2nd parameter,var3 is 3rd parameter)
     try:
         # Log start
         if logInfo == "true":
             loggingFunction(logFile,"start","")
 
         # --------------------------------------- Start of code --------------------------------------- #
-        
+
         # If new cache
         if updateMode == "New":
             # If tile scheme file provided
-            if (len(str(tileScheme)) > 0):        
+            if (len(str(tileScheme)) > 0):
                 arcpy.AddMessage("Creating new cache...")
                 # Create cache folders
                 arcpy.CreateMapServerCache_server(mapService, tileScheme, "PREDEFINED", "", "", "", "", "", "0 0", "", "", "0", "COMPACT")
@@ -49,50 +49,50 @@ def mainFunction(mapService,updateMode,tileScheme): # Get parameters from ArcGIS
                 arcpy.ManageMapServerCacheTiles_server(mapService, "", "RECREATE_ALL_TILES", "", "", "", "WAIT")
             else:
                 arcpy.AddError("Please provide a tiling scheme file for the cache...")
-                
+
         # If existing cache to create all tiles
         if updateMode == "Existing - Recreate All Tiles":
             # Rebuild the map cache
             arcpy.AddMessage("Rebuilding cache...")
-            # Start caching process            
+            # Start caching process
             arcpy.ManageMapServerCacheTiles_server(mapService, "", "RECREATE_ALL_TILES", "", "", "", "WAIT")
-            
+
         # If existing cache to create empty tiles
         if updateMode == "Existing - Recreate Empty Tiles":
             # Rebuild the map cache
             arcpy.AddMessage("Rebuilding cache...")
-            # Start caching process            
+            # Start caching process
             arcpy.ManageMapServerCacheTiles_server(mapService, "", "RECREATE_EMPTY_TILES", "", "", "", "WAIT")
-      
-        # --------------------------------------- End of code --------------------------------------- #  
-            
-        # If called from gp tool return the arcpy parameter   
+
+        # --------------------------------------- End of code --------------------------------------- #
+
+        # If called from gp tool return the arcpy parameter
         if __name__ == '__main__':
             # Return the output if there is any
             if output:
                 arcpy.SetParameterAsText(1, output)
-        # Otherwise return the result          
+        # Otherwise return the result
         else:
             # Return the output if there is any
             if output:
-                return output      
+                return output
         # Log start
         if logInfo == "true":
-            loggingFunction(logFile,"end","")        
+            loggingFunction(logFile,"end","")
         pass
     # If arcpy error
     except arcpy.ExecuteError:
         # Show the message
-        arcpy.AddError(arcpy.GetMessages(2))        
+        arcpy.AddError(arcpy.GetMessages(2))
         # Log error
-        if logInfo == "true":  
+        if logInfo == "true":
             loggingFunction(logFile,"error",arcpy.GetMessages(2))
     # If python error
     except Exception as e:
         # Show the message
-        arcpy.AddError(e.args[0])          
+        arcpy.AddError(e.args[0])
         # Log error
-        if logInfo == "true":         
+        if logInfo == "true":
             loggingFunction(logFile,"error",e.args[0])
 # End of main function
 
@@ -101,7 +101,7 @@ def loggingFunction(logFile,result,info):
     #Get the time/date
     setDateTime = datetime.datetime.now()
     currentDateTime = setDateTime.strftime("%d/%m/%Y - %H:%M:%S")
-    
+
     # Open log file to log message and time/date
     if result == "start":
         with open(logFile, "a") as f:
@@ -112,19 +112,19 @@ def loggingFunction(logFile,result,info):
             f.write("---" + "\n")
     if result == "warning":
         with open(logFile, "a") as f:
-            f.write("\n" + "Warning: " + info)               
+            f.write("\n" + "Warning: " + info)
     if result == "error":
         with open(logFile, "a") as f:
             f.write("\n" + "Process ended at " + currentDateTime + "\n")
-            f.write("Error: " + info + "\n")        
+            f.write("Error: " + info + "\n")
             f.write("---" + "\n")
         # Send an email
         if sendEmail == "true":
             arcpy.AddMessage("Sending email...")
             # Server and port information
-            smtpserver = smtplib.SMTP("smtp.gmail.com",587) 
+            smtpserver = smtplib.SMTP("smtp.gmail.com",587)
             smtpserver.ehlo()
-            smtpserver.starttls() 
+            smtpserver.starttls()
             smtpserver.ehlo
             # Login with sender email address and password
             smtpserver.login(emailUser, emailPassword)
@@ -133,11 +133,11 @@ def loggingFunction(logFile,result,info):
             message = header + '\n' + emailMessage + '\n' + '\n' + info
             # Send the email and close the connection
             smtpserver.sendmail(emailUser, emailTo, message)
-            smtpserver.close()                
-# End of logging function     
+            smtpserver.close()
+# End of logging function
 
 # This test allows the script to be used from the operating
-# system command prompt (stand-alone), in a Python IDE, 
+# system command prompt (stand-alone), in a Python IDE,
 # as a geoprocessing script tool, or as a module imported in
 # another script
 if __name__ == '__main__':
@@ -145,4 +145,4 @@ if __name__ == '__main__':
     argv = tuple(arcpy.GetParameterAsText(i)
         for i in range(arcpy.GetArgumentCount()))
     mainFunction(*argv)
-    
+
